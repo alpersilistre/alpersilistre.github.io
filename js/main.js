5577b47bf3ddfb4f05f0f4af5208b8f8759ca1a9
Alper Silistre <alpersilistre@gmail.com>
2017-08-17 22:47:08 +0300

    Performance Optimization and service worker added

--- js/main.js
+++ js/main.js
@@ -1,25 +1,11 @@
 jQuery(document).ready(function($) {
-
-
-    /*======= Skillset *=======*/
-    
-    
     $('.level-bar-inner').css('width', '0');
-    
     $(window).on('load', function() {
-
         $('.level-bar-inner').each(function() {
-        
-            var itemWidth = $(this).data('level');
-            
+            var itemWidth = $(this).data('level');        
             $(this).animate({
                 width: itemWidth
-            }, 800);
-            
+            }, 800);            
         });
-
     });
-   
-    
-
 });
---
+++ sw.js
@@ -0,0 +1,26 @@
+importScripts('/cache-polyfill.js');
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('alpersilistre').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/index.html?homescreen=1',
+       '/?homescreen=1',
+       '/css/styles.css',
+       '/js/min/main-min.js',
+       '/images/profile-img.png'
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(event) {  
+  console.log(event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );  
+});
